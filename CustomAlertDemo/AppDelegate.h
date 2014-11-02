1fa4e72ce2354465f3d3c1f30dfd49f2ec8d132a
Ajith Kumar <ajithmn89@gmail.com>
2014-11-02 20:40:02 +0530

    Initial Commit

---
+++ CustomAlertDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CustomAlertDemo
+//
+//  Created by Ajith Kumar on 02/11/14.
+//  Copyright (c) 2014 Ajith Kumar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CustomAlertDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CustomAlertDemo
+//
+//  Created by Ajith Kumar on 02/11/14.
+//  Copyright (c) 2014 Ajith Kumar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
