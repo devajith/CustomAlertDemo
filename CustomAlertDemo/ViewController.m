fa83a9bd108b8b09a51ab586a7ef12295b4d3529
devajith <dev.ajith89@gmail.com>
2014-11-06 23:07:22 +0530

    First commit

--- CustomAlertDemo/ViewController.m
+++ CustomAlertDemo/ViewController.m
@@ -7,8 +7,9 @@
 //
 
 #import "ViewController.h"
+#import "CustomAlert.h"
 
-@interface ViewController ()
+@interface ViewController ()<AlertActions>
 
 @end
 
@@ -22,6 +23,28 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)one:(id)sender {
+    
+    CustomAlert *alert=[[CustomAlert alloc] initWithMessage:@"Error occurred during initialization. \n Please Try again." andCancelButtonTitle:@"OK Buddy" withAlertImage:[UIImage imageNamed:@"3.png"]];
+    alert.delegate=self;
+    alert.tag=101;
+    [alert showAlert];
+    
+}
+
+- (IBAction)two:(id)sender {
+    
+    CustomAlert *alert=[[CustomAlert alloc] initWithMessage:@"Are you sure you want to quit ?" andLeftButtonTitle:@"YES" andRightButtonTitle:@"NO" withAlertImage:[UIImage imageNamed:@"3.png"]];
+    alert.delegate=self;
+    alert.tag=102;
+    [alert showAlert];
+}
+
+-(void)alert:(CustomAlert *)alert ButtonclickedAtIndex:(NSInteger)index{
+    
+    NSLog(@"Aler with tag = %ld and clicked button at %ld",(long)alert.tag,(long)index);
 }
 
 @end
